001858496d3a0c72b7b5b060536ce1ba744e94ab
zokaiG <futlouco7@gmail.com>
2020-04-16 14:44:20 -0400

    Completed Lesson 4 - Group Transitions

--- animating-vue/src/router.js
+++ animating-vue/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Modal from './views/Modal.vue'
+import List from './views/List.vue'
 
 Vue.use(Router)
 
@@ -14,13 +15,9 @@ export default new Router({
       component: Modal
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/list',
+      name: 'list',
+      component: List
     }
   ]
 })
