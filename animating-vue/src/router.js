9d762e3cde29a1167150c16bf3fa1fba35727ad6
zokaiG <futlouco7@gmail.com>
2020-04-16 15:50:07 -0400

    Completed Lesson 5 - Javascript Hooks + Velocity

--- animating-vue/src/router.js
+++ animating-vue/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Modal from './views/Modal.vue'
 import List from './views/List.vue'
+import Drawer from './views/Drawer.vue'
+import Cards from './views/Cards.vue'
 
 Vue.use(Router)
 
@@ -18,6 +20,16 @@ export default new Router({
       path: '/list',
       name: 'list',
       component: List
+    },
+    {
+      path: '/drawer',
+      name: 'drawer',
+      component: Drawer
+    },
+    {
+      path: '/cards',
+      name: 'cards',
+      component: Cards
     }
   ]
 })
