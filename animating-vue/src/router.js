526c29dd8b87fdd1b5e2dcae08d225bd2d208c60
zokaiG <futlouco7@gmail.com>
2020-04-16 16:09:42 -0400

    Completed Lesson 6 - Intro to GSAP3

--- animating-vue/src/router.js
+++ animating-vue/src/router.js
@@ -4,6 +4,8 @@ import Modal from './views/Modal.vue'
 import List from './views/List.vue'
 import Drawer from './views/Drawer.vue'
 import Cards from './views/Cards.vue'
+import Simple from './views/Simple.vue'
+import Stagger from './views/Stagger.vue'
 
 Vue.use(Router)
 
@@ -30,6 +32,16 @@ export default new Router({
       path: '/cards',
       name: 'cards',
       component: Cards
+    },
+    {
+      path: '/simple',
+      name: 'simple',
+      component: Simple
+    },
+    {
+      path: '/stagger',
+      name: 'stagger',
+      component: Stagger
     }
   ]
 })
